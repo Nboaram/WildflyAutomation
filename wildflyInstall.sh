998c9062b6bcd18b8bfca0958ef72b7ec9ae8cfa
nboaram <nboaram1@hotmail.co.uk>
2019-06-28 11:36:50 +0100

    actually Removed Maven

--- wildflyInstall.sh
+++ wildflyInstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt update
 sudo apt install openjdk-8-jdk -y
-sudo apt install maven -y
 sudo curl -o wildfly.tar https://download.jboss.org/wildfly/17.0.0.Final/wildfly-17.0.0.Final.tar.gz
 tar -xvf wildfly.tar
 rm wildfly.tar
