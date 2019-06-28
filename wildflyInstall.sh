4ee04ca79e1861ab2771577fcee0d2c5c9184961
nboaram <nboaram1@hotmail.co.uk>
2019-06-28 14:46:27 +0100

    forced deletion

--- wildflyInstall.sh
+++ wildflyInstall.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install openjdk-8-jdk -y
 sudo curl -o wildfly.tar https://download.jboss.org/wildfly/17.0.0.Final/wildfly-17.0.0.Final.tar.gz
 tar -xvf wildfly.tar
-rm wildfly.tar
+rm -f wildfly.tar
 cd wildfly-17.0.0.Final/bin/
 ./standalone.sh
 
